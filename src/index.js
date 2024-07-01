125122da111d7c4e511ac21845b426f40830757f
akasharyanofficial7 <akasharyangupta7@gmail.com>
2024-07-01 18:36:37 +0530

    git check

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 const { PORT } = require("./config/configServer");
 
-console.log("kdwjcvjh ");
+console.log(" hi ");
 app.listen(PORT, () => {
   console.log(` the port is ${PORT}`);
 });
