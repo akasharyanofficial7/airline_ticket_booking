7e333e620343d002a99a75f85491f57d0abce6e5
akasharyanofficial7 <akasharyangupta7@gmail.com>
2024-07-01 18:32:06 +0530

    setup the server

---
+++ src/index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+const { PORT } = require("./config/configServer");
+
+console.log("kdwjcvjh ");
+app.listen(PORT, () => {
+  console.log(` the port is ${PORT}`);
+});
