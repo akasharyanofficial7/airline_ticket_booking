16994194caaa5844f2e4e3966a24272c342d20b5
akasharyanofficial7 <akasharyangupta7@gmail.com>
2024-07-01 22:34:17 +0530

    completed basis sequelize and create database

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 const { PORT } = require("./config/configServer");
 
-console.log("  hello hi l.glyvkydcjycyj  ");
+console.log("  hello hi l.glyvkydcjycyj  hvhgv ");
 app.listen(PORT, () => {
   console.log(` the port is ${PORT}`);
 });
